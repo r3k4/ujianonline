ae8609c441f5756b1c46ce911e1519690292244e
reka prihatanto <rey.barrolz@gmail.com>
2015-10-28 11:40:01 +0700

    perbaikan tampilan landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,11 +11,11 @@
 	<script src="{{ elixir('js/main.js') }}"></script>
 
   </head>
-  <body style="background: url(/img/header.jpg) ; ">
+  <body>
 
-    <nav class="navbar navbar-landing">
+<div class="landing-atas">
+    <nav class="navbar navbar-default">
       <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
             <span class="sr-only">Toggle navigation</span>
@@ -23,10 +23,10 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Brand</a>
+          <a class="navbar-brand" href="#">
+            <i class="fa fa-cloud-upload"></i>
+          </a>
         </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
             <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
@@ -35,12 +35,17 @@
           </ul>
 
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">Link</a></li>
+            <li>
+               <button class='btn btn-success '> <i class="fa fa-lock"></i> LOGIN</button>
 
+            </li>
           </ul>
-        </div><!-- /.navbar-collapse -->
-      </div><!-- /.container-fluid -->
+        </div>
+      </div>
     </nav>
+
+
+</div>
 
   <div class="container animated fadeIn">
     <h1>  Welcome to Edmodo</h1>
