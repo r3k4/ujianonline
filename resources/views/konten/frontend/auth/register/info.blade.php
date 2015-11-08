6144877cb1203d9153e998a928e30385470cec63
reka prihatanto <rey.barrolz@gmail.com>
2015-11-09 06:05:21 +0700

    edit form registrasi

--- resources/views/konten/frontend/auth/register/info.blade.php
+++ resources/views/konten/frontend/auth/register/info.blade.php
@@ -1,5 +1,5 @@
 <div class="alert alert-info">
-	<i class="fa fa-warning"></i> Info :
+	<h3> <i class="fa fa-warning"></i> Info</h3>
 	<hr>
 	 Pendaftaran user bisa dilakukan siapa pun, 
 	akan tetapi butuh persetujuan oleh admin terlebih dahulu untuk aktivasi user.
